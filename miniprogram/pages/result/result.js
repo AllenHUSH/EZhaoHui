8c575394204c0f631d662a4c1147db0459437dc6
1002861502@qq.com <1002861502@qq.com>
2020-02-16 22:57:14 +0800

    增加了长按复制的功能，增加判断为空，修改了样式

--- miniprogram/pages/result/result.js
+++ miniprogram/pages/result/result.js
@@ -5,7 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
+    li:"dd"
+  },
+  onLongpress:function(event){
+    console.log("ddd");
+    var url=event.currentTarget.dataset.url;
     
+    wx.setClipboardData({
+      data: url,
+      success:function(res){
+        wx.getClipboardData({
+          success:function(res){
+            console.log(res.data);
+          }
+        })
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -16,53 +31,61 @@ Page({
 "bg-brown"];
     var result_content=[
       {
+        iscolor:false,//判断是否有颜色
         color: arr[Math.floor((Math.random()*arr.length))],
-        picture:"/images/result/u=2399821824,1913911900&fm=26&gp=0.jpg",
+        picture:'',//判断是否有图片
         _id:'kdgjrghurgnurgg',
         city:'长春市',
         company:'东师理想',
         creat_time:'Sun Feb 16 2020',
         end_time:'Sun Feb 16 2020',
         edu_back:'本科 计算机',
-        info:'待遇：薪资80/天 不包吃住 无转正',
+        info:'薪资80/天 不包吃住 无转正',
         province:'吉林省',
         title:'东师理想春季招聘（20补招/21实习）',
+        url:'jmjimni'
       },{
+        iscolor:false,
         color: arr[Math.floor((Math.random()*arr.length))],
         picture:'',
         _id:'kdgjrghurgnurgg',
-        city:'长春市',
-        company:'东师理想',
+        city:'',
+        company:'中国联通移动',
         creat_time:'Sun Feb 16 2020',
         end_time:'Sun Feb 16 2020',
         edu_back:'本科 计算机',
-        info:'待遇：薪资80/天 不包吃住 无转正',
-        province:'吉林省',
-        title:'东师理想春季招聘（20补招/21实习）',
+        info:'20-40k',
+        province:'上海',
+        title:'中国移动招实习生',
+        url:'jjjjj'
       },{
+        iscolor:true,
         color: arr[Math.floor((Math.random()*arr.length))],
         picture:'',
         _id:'kdgjrghurgnurgg',
-        city:'长春市',
-        company:'东师理想',
+        city:'',
+        company:'中国联通移动',
         creat_time:'Sun Feb 16 2020',
         end_time:'Sun Feb 16 2020',
         edu_back:'本科 计算机',
-        info:'待遇：薪资80/天 不包吃住 无转正',
-        province:'吉林省',
-        title:'东师理想春季招聘（20补招/21实习）',
+        info:'20-40k',
+        province:'上海',
+        title:'中国移动招实习生',
+        url:'jjjjj'
       },{
+        iscolor:true,
         color: arr[Math.floor((Math.random()*arr.length))],
         picture:"/images/result/u=2399821824,1913911900&fm=26&gp=0.jpg",
         _id:'kdgjrghurgnurgg',
-        city:'长春市',
+        city:'',
         company:'东师理想',
         creat_time:'Sun Feb 16 2020',
         end_time:'Sun Feb 16 2020',
         edu_back:'本科 计算机',
-        info:'待遇：薪资80/天 不包吃住 无转正',
-        province:'吉林省',
+        info:'薪资80/天 不包吃住 无转正',
+        province:'',
         title:'东师理想春季招聘（20补招/21实习）',
+        url:'jjjjj'
       }
     ]
     this.setData({result_content});
