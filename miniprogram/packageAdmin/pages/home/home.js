550b6a4d97585a39f489cb4c791d363cbfb5a243
allenhush <1324816813@qq.com>
2020-02-17 00:38:07 +0800

    fix:修复了app.json丢失的问题byAllen

--- miniprogram/packageAdmin/pages/home/home.js
+++ miniprogram/packageAdmin/pages/home/home.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/admin/admin_home/admin_home.js
+// packageAdmin/pages/home/home.js
 Page({
 
   /**
@@ -6,37 +6,37 @@ Page({
    */
   data: {
     elements: [{
-        title: '待处理',
-        name: 'pending',
-        color: 'cyan',
-        icon: 'writefill'
-      },
-      {
-        title: '未通过',
-        name: 'not pass',
-        color: 'blue',
-        icon: 'roundclosefill'
-      },
-      {
-        title: '已过期',
-        name: 'out of date',
-        color: 'purple',
-        icon: 'timefill',
-      },
-      {
-        title: '公布中',
-        name: 'publishing',
-        color: 'mauve',
-        icon: 'settingsfill'
-      },
-      {
-        title: '退出',
-        name: 'Exit',
-        color: 'pink',
-        icon: 'back',
-        url:"/pages/index/index",
-        type:"switchTab"
-      }
+      title: '待处理',
+      name: 'pending',
+      color: 'cyan',
+      icon: 'writefill'
+    },
+    {
+      title: '未通过',
+      name: 'not pass',
+      color: 'blue',
+      icon: 'roundclosefill'
+    },
+    {
+      title: '已过期',
+      name: 'out of date',
+      color: 'purple',
+      icon: 'timefill',
+    },
+    {
+      title: '公布中',
+      name: 'publishing',
+      color: 'mauve',
+      icon: 'settingsfill'
+    },
+    {
+      title: '退出',
+      name: 'Exit',
+      color: 'pink',
+      icon: 'back',
+      url: "/pages/index/index",
+      type: "switchTab"
+    }
     ],
     processedElements: [
       {
@@ -72,24 +72,24 @@ Page({
     ],
     url: ""
   },
-  formSubmit: function(e) {  
-    console.log('form发生了submit事件，携带数据为：', e.detail.value);  
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
     let {
       url
-    } = e.detail.value;  
-    this.setData({    
+    } = e.detail.value;
+    this.setData({
       url
-    }) 
+    })
     wx.cloud.callFunction({
       // 要调用的云函数名称
       name: 'insertUrl',
       // 传递给云函数的event参数
       data: {
-        target_url:this.data.url,
-        username:"as"
+        target_url: this.data.url,
+        username: "as"
       }
     }).then(res => {
-      console.log(res) 
+      console.log(res)
     }).catch(err => {
       console.log(err)
     })
@@ -97,56 +97,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
--- miniprogram/pages/result/result.js
+++ miniprogram/pages/result/result.js
@@ -7,12 +7,65 @@ Page({
   data: {
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var arr = ["bg-red", "bg-orange", "bg-yellow",
+      "bg-purple", "bg-olive", "bg-green", "bg-cyan", "bg-blue",
+      "bg-brown"];
+    var result_content = [
+      {
+        color: arr[Math.floor((Math.random() * arr.length))],
+        picture: "/images/result/u=2399821824,1913911900&fm=26&gp=0.jpg",
+        _id: 'kdgjrghurgnurgg',
+        city: '长春市',
+        company: '东师理想',
+        creat_time: 'Sun Feb 16 2020',
+        end_time: 'Sun Feb 16 2020',
+        edu_back: '本科 计算机',
+        info: '待遇：薪资80/天 不包吃住 无转正',
+        province: '吉林省',
+        title: '东师理想春季招聘（20补招/21实习）',
+      }, {
+        color: arr[Math.floor((Math.random() * arr.length))],
+        picture: '',
+        _id: 'kdgjrghurgnurgg',
+        city: '长春市',
+        company: '东师理想',
+        creat_time: 'Sun Feb 16 2020',
+        end_time: 'Sun Feb 16 2020',
+        edu_back: '本科 计算机',
+        info: '待遇：薪资80/天 不包吃住 无转正',
+        province: '吉林省',
+        title: '东师理想春季招聘（20补招/21实习）',
+      }, {
+        color: arr[Math.floor((Math.random() * arr.length))],
+        picture: '',
+        _id: 'kdgjrghurgnurgg',
+        city: '长春市',
+        company: '东师理想',
+        creat_time: 'Sun Feb 16 2020',
+        end_time: 'Sun Feb 16 2020',
+        edu_back: '本科 计算机',
+        info: '待遇：薪资80/天 不包吃住 无转正',
+        province: '吉林省',
+        title: '东师理想春季招聘（20补招/21实习）',
+      }, {
+        color: arr[Math.floor((Math.random() * arr.length))],
+        picture: "/images/result/u=2399821824,1913911900&fm=26&gp=0.jpg",
+        _id: 'kdgjrghurgnurgg',
+        city: '长春市',
+        company: '东师理想',
+        creat_time: 'Sun Feb 16 2020',
+        end_time: 'Sun Feb 16 2020',
+        edu_back: '本科 计算机',
+        info: '待遇：薪资80/天 不包吃住 无转正',
+        province: '吉林省',
+        title: '东师理想春季招聘（20补招/21实习）',
+      }
+    ]
+    this.setData({ result_content });
   },
 
   /**
