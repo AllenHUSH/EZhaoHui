aa1b354e528b131302e7e8f8aa8df105232a4c97
1002861502@qq.com <1002861502@qq.com>
2020-02-16 18:53:59 +0800

    核心代码已将写完

--- miniprogram/pages/result/result.js
+++ miniprogram/pages/result/result.js
@@ -20,6 +20,7 @@ Page({
     var result_content=[
       {
         color: arr[Math.floor((Math.random()*arr.length))],
+        picture:"/images/result/u=2399821824,1913911900&fm=26&gp=0.jpg",
         _id:'kdgjrghurgnurgg',
         city:'长春市',
         company:'东师理想',
@@ -31,6 +32,7 @@ Page({
         title:'东师理想春季招聘（20补招/21实习）',
       },{
         color: arr[Math.floor((Math.random()*arr.length))],
+        picture:'',
         _id:'kdgjrghurgnurgg',
         city:'长春市',
         company:'东师理想',
@@ -42,6 +44,7 @@ Page({
         title:'东师理想春季招聘（20补招/21实习）',
       },{
         color: arr[Math.floor((Math.random()*arr.length))],
+        picture:'',
         _id:'kdgjrghurgnurgg',
         city:'长春市',
         company:'东师理想',
@@ -53,17 +56,7 @@ Page({
         title:'东师理想春季招聘（20补招/21实习）',
       },{
         color: arr[Math.floor((Math.random()*arr.length))],
-        _id:'kdgjrghurgnurgg',
-        city:'长春市',
-        company:'东师理想',
-        creat_time:'Sun Feb 16 2020',
-        end_time:'Sun Feb 16 2020',
-        edu_back:'本科 计算机',
-        info:'待遇：薪资80/天 不包吃住 无转正',
-        province:'吉林省',
-        title:'东师理想春季招聘（20补招/21实习）',
-      },{
-        color: arr[Math.floor((Math.random()*arr.length))],
+        picture:"/images/result/u=2399821824,1913911900&fm=26&gp=0.jpg",
         _id:'kdgjrghurgnurgg',
         city:'长春市',
         company:'东师理想',
