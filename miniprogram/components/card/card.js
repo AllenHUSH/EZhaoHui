c7aa504c65c1d58fff8a364bfb6b5f900d70eae3
yanglanq <2873435684@qq.com>
2020-02-18 18:57:52 +0800

    修改组件card

--- miniprogram/components/card/card.js
+++ miniprogram/components/card/card.js
@@ -1,93 +1,46 @@
 // components/card/card.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-// 从外面传进来
-    iscolor:{
-      type:Boolean,
-      value:false,
-    },//判断是否有颜色
-    color:{
-      type: String,
-      value: '',
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        // 从外面传进来
+        card: {
+            type: Object,
+            default: {}
+        },
+		number:
     },
-    picture: {
-      type: String,
-      value: '',
-    },//判断是否有图片
-    _id: {
-      type: String,
-      value: '',
-    },
-    city: {
-      type: String,
-      value: '',
-    },
-    company: {
-      type: String,
-      value: '',
-    },
-    creat_time: {
-      type: String,
-      value: '',
-    },
-    end_time: {
-      type: String,
-      value: '',
-    },
-    edu_back: {
-      type: String,
-      value: '',
-    },
-    info: {
-      type: String,
-      value: '',
-    },
-    province: {
-      type: String,
-      value: '',
-    },
-    title: {
-      type: String,
-      value: '',
-    },
-    url: {
-      type: String,
-      value: '',
-    },
-    
-    
-    
-  },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-     
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+		color: ["bg-gradual-red", "bg-gradual-orange", "bg-gradual-green", "bg-gradual-blue", "bg-gradual-purple", "bg-gradual-pink"]
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    //长按复制
-    onLongpress:function(event){
-      console.log("ddd");
-      var url=event.currentTarget.dataset.url;
-      wx.setClipboardData({
-        data: url,
-        success:function(res){
-          wx.getClipboardData({
-            success:function(res){
-              console.log(res.data);
-            }
-          })
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        //长按复制
+        copyUrl(event) {
+            var url = event.currentTarget.dataset.url;
+			wx.setClipboardData({
+				data: this.card.url,//推送链接
+				success: function (res) {
+					wx.getClipboardData({
+						success: function (res) {
+							wx.showToast({
+								title: '复制链接成功'
+							})
+						}
+					})
+				}
+			})
         }
-      })
+		,color(){
+			return Math.floor(Math.random()*6);
+		}
     }
-
-  }
 })
