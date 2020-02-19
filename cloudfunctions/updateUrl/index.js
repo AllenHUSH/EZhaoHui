e5ef0c5c143f9db2d1c4b5d9abaaa1a6b592da95
VigoWeiGou <49744092+VigoWeiGou@users.noreply.github.com>
2020-02-19 23:02:09 +0800

    更新 updateUrl 云函数
    管理员通过 _id 更新用户的信息

--- cloudfunctions/updateUrl/index.js
+++ cloudfunctions/updateUrl/index.js
@@ -6,21 +6,45 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  if(event.id == null|| event.id.leng == 0){
+    return {
+      data: {
+        info: 0,
+        msg: "id_empty"
+      }
+    }
+  }
   await db.collection('t_url').doc(event.id).update({
       data:{
-        url: event.target_url,
         city: event.city,
         company: event.company,
-        creat_time: db.serverDate(),
+        create_time: new Date(JSON.parse(event.create_time)),
+        salary: event.salary,
+        menbers: event.menbers,
         edu_back: event.edu_back,
-        end_time: event.end_time,
+        end_time: new Date(JSON.parse(event.end_time)),
         info: event.info,
+        picture: event.picture,
         province: event.province,
         state: event.state,
         title: event.title,
         url: event.url,
-        username: event.username
+        username: event.username,
+      },
+    success: function (res) {
+      return {
+        info: 1,
+        msg: "update_success"
       }
+    },
+    fail: function (res) {
+      return {
+        data: {
+          info: 0,
+          msg: "update_fail"
+        }
+      }
+    }
   })
   return {
     msg: 'ok'
